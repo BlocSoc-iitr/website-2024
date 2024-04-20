be59c9b5676273e12d50972f487bc8fdeaf53d09
Samarendra Gouda <samarendragouda@gmail.com>
2024-04-21 00:15:50 +0530

    fix: WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,38 @@
 <template>
-  <div class="flex justify-center items-start w-full">
-    <div class="max-w-2xl w-full mt-4">
+  <div class="flex items-start justify-center w-full">
+    <div class="w-full max-w-2xl mt-4">
       <header class="flex flex-col w-full">
         <div
-          class="flex flex-row w-full justify-start items-center border-gray-200 border-b pb-4"
+          class="flex flex-row items-center justify-start w-full pb-4 border-b border-gray-200"
         >
-          <SvgoLogo class="h-16 w-24" />
+          <SvgoLogo class="w-24 h-16" />
           <div class="flex flex-col">
-            <h2 class="font-semibold text-2xl">Blockchain Society</h2>
+            <h2 class="text-2xl font-semibold">Blockchain Society</h2>
             <h3>Indian Institute of Technology Roorkee</h3>
           </div>
         </div>
         <div
-          class="flex flex-row text-gray-500 border-b border-gray-200 text-xs"
+          class="flex flex-row text-xs text-gray-500 border-b border-gray-200"
         >
-        <div class="overflow-x-auto flex flex-row w-full justify-start items-center">
+        <div class="flex flex-row items-center justify-start w-full overflow-x-auto">
           <NuxtLink to="/" class="p-4 text-[#0C21D6]">Home</NuxtLink>
           <NuxtLink to="/projects" class="p-4">Projects</NuxtLink>
           <NuxtLink to="/events" class="p-4">Events</NuxtLink>
           <NuxtLink to="/achievements" class="p-4">Achievements</NuxtLink>
           <NuxtLink to="/resources" class="p-4">Resources</NuxtLink>
           <NuxtLink to="/team" class="p-4">Team</NuxtLink>
-          <NuxtLink to="/contact" class="p-4">Contact</NuxtLink>
         </div>
         </div>
       </header>
       <slot />
-      <footer class="mt-8 border-t border-gray- p-4 text-gray-400 text-xs">
-        &copy; 2024 Blockchain Society, IIT Roorkee
+      <footer class="flex flex-row items-center justify-between p-4 mt-8 text-xs text-gray-400 border-t border-gray-200">
+        <div>&copy; 2024 Blockchain Society, IIT Roorkee</div>
+        <div class="flex flex-row items-center justify-center gap-2">
+          <SvgoEmail class="w-4 h-4" />
+          <SvgoGithub class="w-4 h-4" />
+          <SvgoTwitter class="w-4 h-4" />
+          <SvgoLinkedin class="w-4 h-4" />
+        </div>
       </footer>
     </div>
   </div>
--- pages/achievements.vue
+++ pages/achievements.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="mt-12 px-4">
-       <h2 class="text-2xl font-semibold">Achievements</h2>
+  <div class="px-4 mt-12">
+    <h2 class="text-2xl font-semibold">Achievements</h2>
+  </div>
+  <div class="mt-4">
+    <div class="flex flex-row items-start justify-start gap-4">
+      <div
+        class="flex flex-col w-[20px] h-full justify-center items-center gap-3 py-2"
+      >
+        <div class="w-[8px] h-[8px] rounded-full bg-gray-500" />
+        <div class="h-full min-h-[30px] border-r border-gray-500" />
+      </div>
+      <div class="flex flex-row items-center justify-center h-full gap-2">
+        <div class="content">
+          <div class="text-sm font-medium">Winners at ETH Mumbai Hackathon</div>
+        </div>
+      </div>
     </div>
+    <div class="flex flex-row items-start justify-start gap-4">
+      <div
+        class="flex flex-col w-[20px] h-full justify-center items-center gap-3 py-2"
+      >
+        <div class="w-[8px] h-[8px] rounded-full bg-gray-500" />
+        <div class="h-full min-h-[30px] border-r border-gray-500" />
+      </div>
+      <div class="flex flex-row items-center justify-center h-full gap-2">
+        <div class="content">
+          <div class="text-sm font-medium">Winners at ETH Mumbai Hackathon</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
