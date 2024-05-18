7f26960f9861b337822b40d15a035177fe8e6fc1
Samarendra Gouda <samarendragouda@gmail.com>
2024-05-18 17:49:39 +0530

    feat: init

---
+++ components/Admin/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex flex-row items-center justify-between w-full h-20 px-10 py-4 bg-slate-100"
+  >
+    <div class="text-2xl font-semibold">Overview</div>
+    <div class="flex flex-row">
+      <SvgoProfileMale class="text-gray-400 w-14 h-14" />
+      <div class="flex flex-col items-start justify-center gap-0.5">
+        <div class="text-sm font-semibold">Samarendra Gouda</div>
+        <div class="text-xs font-medium">Superuser</div>
+      </div>
+    </div>
+  </div>
+</template>
